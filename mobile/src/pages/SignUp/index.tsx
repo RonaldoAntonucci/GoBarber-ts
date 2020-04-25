55b774d2553eeb4e36f31a4625565394b9db4b41
Ronaldo Antonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2020-04-25 01:52:03 -0300

    (mobile): pagina de signUp

--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -1,13 +1,59 @@
 import React from 'react';
-import { Text } from 'react-native';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title, BackToSign, BackToSignText } from './styles';
 
 const SignUp: React.FC = () => {
   return (
-    <Container>
-      <Text>SignUp</Text>
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                console.log('botao');
+              }}
+            >
+              Entrar
+            </Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSign onPress={() => {}}>
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSignText>Voltar para logon</BackToSignText>
+      </BackToSign>
+    </>
   );
 };
 
