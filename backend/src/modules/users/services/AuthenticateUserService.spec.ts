1f4af6c3f7a47cc2e08c86aca281d23825b720c7
Ronaldo Antonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2020-05-04 16:05:02 -0300

    tests: User Authenticate Service

--- backend/src/modules/users/services/AuthenticateUserService.spec.ts
+++ backend/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,29 +1,32 @@
 import User from '@modules/users/infra/typeorm/entities/User';
+import { uuid } from 'uuidv4';
 
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
-import { uuid } from 'uuidv4';
-import { hash } from 'bcryptjs';
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 
 describe('Authenticate User', () => {
   let service: AuthenticateUserService;
   let repo: FakeUsersRepository;
+  let hashProvider: FakeHashProvider;
 
   beforeEach(() => {
     repo = new FakeUsersRepository();
-    service = new AuthenticateUserService(repo);
+    hashProvider = new FakeHashProvider();
+    service = new AuthenticateUserService(repo, hashProvider);
   });
 
   it('should be able to Authenticate.', async () => {
     const userPassword = '123456';
 
-    jest.spyOn(repo, 'findByEmail').mockImplementation(async () => {
-      const user = new User();
-      const userData = { id: uuid(), password: await hash(userPassword, 8) };
-      Object.assign(user, userData);
-      return user;
+    const user = new User();
+    Object.assign(user, {
+      id: uuid(),
+      password: await hashProvider.generateHash(userPassword),
     });
+
+    jest.spyOn(repo, 'findByEmail').mockImplementation(async () => user);
 
     const auth = await service.run({
       email: 'usermail@email.com',
@@ -33,5 +36,37 @@ describe('Authenticate User', () => {
     expect(auth).toHaveProperty('token');
     expect(auth).toHaveProperty('user');
     expect(auth.user).not.toHaveProperty('password');
+    delete user.password;
+    expect(auth.user).toEqual(user);
+  });
+
+  it('should not be able to Authenticate with non existing user.', async () => {
+    await expect(() =>
+      service.run({
+        email: 'usermail@email.com',
+        password: '123456',
+      }),
+    ).rejects.toEqual(
+      new AppError('Incorrect email/password combination.', 401),
+    );
+  });
+
+  it('should not be able to Authenticate with wrong password.', async () => {
+    const user = new User();
+    Object.assign(user, {
+      id: uuid(),
+      password: await hashProvider.generateHash('123456'),
+    });
+
+    jest.spyOn(repo, 'findByEmail').mockImplementation(async () => user);
+
+    await expect(() =>
+      service.run({
+        email: 'usermail@email.com',
+        password: 'wrong-password',
+      }),
+    ).rejects.toEqual(
+      new AppError('Incorrect email/password combination.', 401),
+    );
   });
 });
--- backend/src/modules/users/services/CreateUserService.spec.ts
+++ backend/src/modules/users/services/CreateUserService.spec.ts
@@ -3,36 +3,43 @@ import User from '@modules/users/infra/typeorm/entities/User';
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import CreateUserService from '@modules/users/services/CreateUserService';
 import AppError from '@shared/errors/AppError';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 
 describe('Create User', () => {
   let service: CreateUserService;
   let repo: FakeUsersRepository;
+  let hashProvider: FakeHashProvider;
 
   beforeEach(() => {
     repo = new FakeUsersRepository();
-    service = new CreateUserService(repo);
+    hashProvider = new FakeHashProvider();
+    service = new CreateUserService(repo, hashProvider);
   });
 
   it('should be able to create a new user.', async () => {
+    const userPassword = '123456';
     const user = await service.run({
       name: 'teste',
       email: 'email@teste.com',
-      password: '123456',
+      password: userPassword,
     });
 
     expect(user).toBeInstanceOf(User);
     expect(user).toHaveProperty('id');
+    expect(await hashProvider.compareHash(userPassword, user.password)).toBe(
+      true,
+    );
   });
 
   it('should not be able to create a new user with same email from another.', async () => {
     jest.spyOn(repo, 'findByEmail').mockImplementation(async () => new User());
 
-    expect(
+    await expect(() =>
       service.run({
         name: 'teste',
         email: 'email@teste.com',
         password: '123456',
       }),
-    ).rejects.toBeInstanceOf(AppError);
+    ).rejects.toEqual(new AppError('Email address already used.'));
   });
 });
