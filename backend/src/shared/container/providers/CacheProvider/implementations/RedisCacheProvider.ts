67652e2b5b72fe2ad89762cd9b086a7384032f0d
Ronaldo Antonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2020-05-14 18:47:18 -0300

    refatoração do container

---
+++ backend/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -0,0 +1,17 @@
+import Redis, { Redis as RedisClient } from 'ioredis';
+
+import ICacheProvider from '../models/ICacheProvider';
+
+export default class RedisCacheProvider implements ICacheProvider {
+  private client: RedisClient;
+
+  constructor() {
+    this.client = new Redis();
+  }
+
+  public async save(key: string, value: string): Promise<void> {}
+
+  public async recover(key: string): Promise<string> {}
+
+  public async invalidate(key: string): Promise<void> {}
+}
